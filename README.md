c751d631915afdfafebec8b1b29d497c2236025c
CSSERVERDEV <hlloaccp@live.cn>
2019-05-21 14:44:57 +0800

    智能家居大数据项目

---
+++ README.md
@@ -0,0 +1,3 @@
+# IOTBigData
+
+IOTBigData顾名思义就是智能家居大数据项目,首要有用的就是 Spark
---
+++ src/main/java/HelloWord.java
@@ -0,0 +1,10 @@
+/**
+ * HelloWord
+ *
+ * @author zengsong
+ * @version 1.0
+ * @description TODO
+ * @date 2019/5/21 14:44
+ **/
+public class HelloWord {
+}
